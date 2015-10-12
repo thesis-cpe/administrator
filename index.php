21e97dd3e3cf0d14757429757e7753aa18762a92
thesis-cpe <witthayaniphon@hotmail.com>
2015-10-12 15:09:43 +0700

    สคิปล๊อคอิน

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>ระบบจัดการแอปพลิเคชันอุทยานหลวงราชพฤกษ์</title>
+  <title>Authorization Access Only | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.5 -->
@@ -15,17 +14,8 @@
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="plugins/datatables/dataTables.bootstrap.css">
-  
-  
-  
-  <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
-        page. However, you can choose any other skin. Make sure you
-        apply the skin class to the body tag so the changes take effect.
-  -->
- <!-- <link rel="stylesheet" href="dist/css/skins/skin-blue.min.css"> -->
-  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -34,181 +24,69 @@
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 </head>
-
-<body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
-
-  <!-- Main Header -->
-  <header class="main-header">
-
-    <!-- Logo -->
-    <a href="index.php" class="logo">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>R</b>AJ</span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Control Panel</b></span>
-    </a>
-
-    <!-- Header Navbar -->
-        <?php include_once "./include/navbar.html"; ?>
-    <!-- .Header Navbar -->
-      
-  </header>
-  <!-- Left side column. contains the logo and sidebar -->
-  <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-        <?php include_once "./include/sidebar.html";?>
-    <!-- /.sidebar -->
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        กิจกรรม
-        <small>เพิ่ม ลบ แก้ไข กิจกรรม</small>
-      </h1>
-      <ol class="breadcrumb">
-      <!--  <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>  -->
-          <li><a href="add-activity.php"><i class="fa fa- fa-plus"></i>เพิ่มกิจกรรม</a></li>
-      </ol>
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-
-      <!-- Your Page Content Here -->
-      <div class="row">
-          <div class="col-xs-12">
-               <!--นำเข้าหน้ารายการกิจกรรม-->
-            <?php include "./include/data_news.html";?>
-         </div><!-- /.col-xs-12 -->
-      </div><!-- /row -->
-      <!-- .Your Page Content Here -->
-
-    </section>
-    <!-- /.content -->
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a ><b>Authorize Access Only!</b></a>
   </div>
-  <!-- /.content-wrapper -->
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Sign in to start your session</p>
 
-  <!-- Main Footer -->
-  <footer class="main-footer">
-     <?php include_once "./include/footer.html";?>
-  </footer>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Create the tabs -->
-    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-      <li class="active"><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-      <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-    </ul>
-    <!-- Tab panes -->
-    <div class="tab-content">
-      <!-- Home tab content -->
-      <div class="tab-pane active" id="control-sidebar-home-tab">
-        <h3 class="control-sidebar-heading">Recent Activity</h3>
-        <ul class="control-sidebar-menu">
-          <li>
-            <a href="javascript::;">
-              <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-
-              <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-
-                <p>Will be 23 on April 24th</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <!-- /.control-sidebar-menu -->
-
-        <h3 class="control-sidebar-heading">Tasks Progress</h3>
-        <ul class="control-sidebar-menu">
-          <li>
-            <a href="javascript::;">
-              <h4 class="control-sidebar-subheading">
-                Custom Template Design
-                <span class="label label-danger pull-right">70%</span>
-              </h4>
-
-              <div class="progress progress-xxs">
-                <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <!-- /.control-sidebar-menu -->
-
+    <form action="sc-action/login.php" method="post">
+      <div class="form-group has-feedback">
+        <input name="user" type="text" class="form-control" placeholder="username">
+        <span class="fa  fa-user  form-control-feedback"></span>
       </div>
-      <!-- /.tab-pane -->
-      <!-- Stats tab content -->
-      <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
-      <!-- /.tab-pane -->
-      <!-- Settings tab content -->
-      <div class="tab-pane" id="control-sidebar-settings-tab">
-        <form method="post">
-          <h3 class="control-sidebar-heading">General Settings</h3>
-
-          <div class="form-group">
-            <label class="control-sidebar-subheading">
-              Report panel usage
-              <input type="checkbox" class="pull-right" checked>
+      <div class="form-group has-feedback">
+          <input name="pass" type="password" class="form-control" placeholder="password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+         <!--- <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Remember Me
             </label>
-
-            <p>
-              Some information about this general settings option
-            </p>
-          </div>
-          <!-- /.form-group -->
-        </form>
+          </div>  -->
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+        </div>
+        <!-- /.col -->
       </div>
-      <!-- /.tab-pane -->
-    </div>
-  </aside>
-  <!-- /.control-sidebar -->
-  <!-- Add the sidebar's background. This div must be placed
-       immediately after the control sidebar -->
-  <div class="control-sidebar-bg"></div>
+    </form>
+<!--
+    <div class="social-auth-links text-center">
+      <p>- OR -</p>
+      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
+        Facebook</a>
+      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
+        Google+</a>
+    </div>  --ฦ
+    <!-- /.social-auth-links -->
+
+   <!-- <a href="#">I forgot my password</a><br>
+    <a href="register.html" class="text-center">Register a new membership</a> -->
+
+  </div>
+  <!-- /.login-box-body -->
 </div>
-<!-- ./wrapper -->
-
-
-<!-- REQUIRED JS SCRIPTS -->
+<!-- /.login-box -->
 
 <!-- jQuery 2.1.4 -->
 <script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
 <!-- Bootstrap 3.3.5 -->
 <script src="bootstrap/js/bootstrap.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/app.min.js"></script>
-
-<!-- DataTables -->
-<script src="plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="plugins/datatables/dataTables.bootstrap.min.js"></script>
-<!-- SlimScroll -->
-<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="plugins/fastclick/fastclick.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
-
-<!-- Optionally, you can add Slimscroll and FastClick plugins.
-     Both of these plugins are recommended to enhance the
-     user experience. Slimscroll is required when using the
-     fixed layout. -->
+<!-- iCheck -->
+<script src="plugins/iCheck/icheck.min.js"></script>
 <script>
   $(function () {
-    $("#example1").DataTable();
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
     });
   });
 </script>
---
+++ sc-action/login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<?php
+session_start();
+include_once '../include/sc-login.php';
+?>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        /*VALIABLE*/
+        @$user = $_POST['user'];
+        @$pass = $_POST['pass'];
+        $passMd5 = md5($pass);
+        /*check login*/
+        $sqlCheckUser = "SELECT * FROM user WHERE user='$user' AND pass='$passMd5'";
+        $query = $conn->query($sqlCheckUser);
+        
+        $numrow = mysqli_num_rows($query);
+        if($numrow >0) 
+        {
+            echo "success";
+                while ($arrCheckUser = $query->fetch_array())
+                {
+                    $user1 = $arrCheckUser['user'];
+                    $pass1 =  $arrCheckUser['pass'];
+                    $id_user = $arrCheckUser['id_user'];
+                    $name = $arrCheckUser['name'];
+                    $surname =  $arrCheckUser['surname'];
+                    $status = $arrCheckUser['status'];
+                }
+            $_SESSION["user"] = $user1;
+            $_SESSION["user_id"] = $id_user;
+            $_SESSION["name"] = $name;
+            $_SESSION["surname"] = $surname;
+            $_SESSION["status"] = $status;
+            
+        }else
+        {
+            echo "username or password not found"."<br>"; ?>
+        <a href="../index.php">back to login</a>
+            
+  <?php } ?>
+        
+        
+        
+    </body>
+        
+</html>
+<?php mysqli_close($conn); ?>
